fb7c13dc64a9c17618e3538432f9e749342977ae
João Victor Araújo <jotavsevla@gmail.com>
2023-08-02 12:25:19 -0300

    Upando
    precisa continuar revendo

--- exercicioSopa.c
+++ exercicioSopa.c
@@ -1,41 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-    
-double soupServings(int n) {
-    if (n > 4451) {
-        return 1.0; // 0.9999999999999999 podendo assim, arredondar para 1.0
-    }
-    n = (n + 24) / 25; // Arredondamento para cima
-    double memo[n+1][n+1]; // Matriz de memoização
-    for (int i = 0; i <= n; i++) { // Inicialização da matriz
-        for (int j = 0; j <= n; j++) { 
-            memo[i][j] = -1.0; // -1.0 significa que ainda não foi calculado
-        }
-    }
+#include <time.h>
+#include <stdbool.h>
 
-    double dp(int i, int j){
-        if (memo[i][j] >= 0) { // Se já foi calculado, retorna o valor
-            return memo[i][j];
-        }
-        if (i <= 0 && j <= 0) { // Se os dois ficarem vazios ao mesmo tempo, retorna 0.5
-            return 0.5;
-        }
-        if (i <= 0) {
-            return 1.0; // Se a sopa A ficar vazia primeiro, retorna 1.0
-        }
-        if (j <= 0) { // Se a sopa B ficar vazia primeiro, retorna 0.0
-            return 0.0;
-        }
-        memo[i][j] = 0.25 * (dp(i - 4, j) + dp(i - 3, j - 1) + dp(i - 2, j - 2) + dp(i - 1, j - 3));
-        return memo[i][j];
-    }
+int ml1 = 0;
+int ml2 = 25;
+int ml3 = 50;
+int ml4 = 75;
+int ml5 = 100;
 
-    return dp(n, n);
+typedef struct pote{
+    int sopaA, sopaB;
+}pote;
+
+
+void remove (pote* balde, int removeA, int removeB){
+    if (balde->sopaA < removeA || balde->sopaB < removeB)
+        return; 
+    balde->sopaA -= removeA;
+    balde->sopaB -= removeB;
+    return;
 }
+int main (void){
+    int n;
+    scanf("%i", &n);
 
-int main() {
-    int n = 100;
-    double result = soupServings(n);
-    printf("A probabilidade de a sopa A ficar vazia primeiro é: %f\n", result);
+    pote balde;
+    balde.sopaA = balde.sopaB = n;
+
+    time_t t;
+    time(&t);
+    unsigned int op = (unsigned int)t;
+    srand(op);
+    int per100 [3]; 
+    for (int i = 0; i < 3; i++)
+        per100 [i] = 0;
+
+    // 0 A primeiro
+    // 1 A e B juntos
+    // 2 B primeiro
+
+    /*Sirva 100ml de sopa A e 0ml de sopa B ,
+    Sirva 75ml de sopa A e 25ml de sopa B ,    
+    Sirva 50ml de sopa A e 50ml de sopa B , e
+    Sirva 25ml de sopa A e 75ml de sopa B .*/
+
+    for (int i = 0; i < 100; i++){
+        while (balde.sopaA > 0 && balde.sopaB > 0){
+            op = rand() % 4;
+            switch (op){
+                case 0: remove (&balde, ml5, ml1);    // int ml1 0
+                    break;                            // int ml2 25
+                case 1: remove (&balde, ml4, ml2);    // int ml3 50
+                    break;                            // int ml4 75
+                case 2: remove (&balde, ml3, ml3);    // int ml5 100
+                    break;
+                case 3: remove (&balde, ml2, ml4);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (balde.sopaA == 0 && balde.sopaB > 0)
+            per100[0] += 1;
+        if (balde.sopaA == 0 && balde.sopaB == 0)
+            per100 [1] += 1;
+        if (balde.sopaA > 0 && balde.sopaB == 0)
+            per100[2] += 1;
+        
+        balde.sopaA = balde.sopaB = n;
+
+    }
+    printf( "chances onde ml totais sao %i sao de A %i, A e B %i e so B %i ", n, per100[0], per100[1], per100[2]);
+
     return 0;
+
 }
