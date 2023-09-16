7c291264625c715d1af8c473eafb8535e9dc60af
João Victor Araújo <jotavsevla@gmail.com>
2023-09-16 12:14:05 -0300

    mergesort funcional
    Gera valores aleatorios de acordo com o valor de mxN e os ordena

--- c/mergesort.c
+++ c/mergesort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define mxN 5
+#define mxN 20
 
 void imprime(int* vetor){
     for(int i = 0; i < mxN; i++){
@@ -13,41 +13,39 @@ void imprime(int* vetor){
 // indice meio é o último valor da parte esquerda e meio+1 representa o primeiro valor da segunda metade
 // inicio é primeiro posiçao da primeira parte e fim é, efetivamente, a ultima posiçao da segunda parte
 void merge(int vetor [], int inicio, int meio, int fim){
-    int count_inicio = inicio, count_meio = meio+1;
-    int count_aux = 0;
-    int *vet_aux = malloc (sizeof(int)*((fim-inicio)+1));
-    //imprime(vet_aux);
-   
-    for (int i = 0; i <= fim; i++){
-        vet_aux[i] = vetor[i];
-    }
-    // TODO: trocar passagem de referencia sendo atraves do aux para o vetor original
-    while (count_inicio <= meio && count_meio <= fim){
+    int* copia = malloc(sizeof(fim-inicio+1));
+    int count_inicio, count_meio, count_sort;
+    count_inicio = inicio; count_meio = meio + 1; count_sort = inicio;
 
-        if (vet_aux [count_inicio] > vet_aux [count_meio]){
-            vetor[count_aux] = vet_aux[count_meio];
-            count_meio++;
-        }
+    for(int i = 0; i <= fim; ++i)
+        copia[i] = vetor[i];
+    
+    while(count_inicio <= meio && count_meio <= fim){
 
-        else {
-            vetor[count_aux] = vet_aux[count_inicio];
+        if( copia[count_inicio] < copia[count_meio]){
+            vetor[count_sort] =  copia [count_inicio];
             count_inicio++;
         }
-        count_aux++;
-    }
-    while (count_inicio <= meio){
-        vetor[count_aux] = vet_aux[count_inicio];
-        count_inicio++;
-        count_aux++;
+        else{
+            vetor[count_sort] = copia[count_meio];
+            count_meio++;
+        }
+            count_sort++;
 
     }
-    while (count_meio <= fim){
-        vetor[count_aux] = vet_aux[count_meio];
-        count_meio++;
-        count_aux++;
+    while(count_inicio <= meio){
+        vetor[count_sort] =  copia [count_inicio];
+        count_inicio++;
+        count_sort++;
     }
-    imprime(vetor);
-    return ;
+    while (count_meio <= fim){
+        vetor[count_sort] = copia[count_meio];
+        count_meio++;
+        count_sort++;
+    }
+    
+
+    return;
 }
 void merge_aux (int vetor[],int inicio, int fim){
     if(fim - inicio < 1) return;
@@ -75,7 +73,7 @@ int main (void){
     int meio, inicio, fim;
     fim = mxN-1;
     inicio = 0;
-    int* vetor = vetor_controlado();
+    int* vetor = numeros_aleatorios();
     imprime(vetor);
     merge_aux(vetor, inicio, fim);
     imprime(vetor);
